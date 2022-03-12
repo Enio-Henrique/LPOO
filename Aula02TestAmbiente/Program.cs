2b5ea712cc457e0f92c4a989169a2551a6a5a63e
eniot <enio.tec250@gmail.com>
2022-03-11 21:03:41 -0300

    Primeiro commit

---
+++ Aula02TestAmbiente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula02TeteAmbiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
